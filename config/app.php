bf1fc123ef8ccda34e55389314df6bed9e295aa1
samvel_b <samvel.b.atlassian@knowledgecity.pro>
2023-11-04 22:20:14 +0400

    Add socialite w/ Google & Tgl

--- config/app.php
+++ config/app.php
@@ -196,6 +196,7 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
+        SocialiteProviders\Manager\ServiceProvider::class,
 
         /*
          * Application Service Providers...
--- config/services.php
+++ config/services.php
@@ -31,4 +31,16 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'telegram' => [
+      'bot' => env('TELEGRAM_BOT_NAME'),
+      'client_id' => null,
+      'client_secret' => env('TELEGRAM_TOKEN'),
+      'redirect' => env('TELEGRAM_REDIRECT_URI'),
+    ],
+
+    'google' => [
+      'client_id' => env('GOOGLE_CLIENT_ID'),
+      'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+      'redirect' => env('GOOGLE_REDIRECT_URI')
+    ],
 ];
